f3340cad901a1fab0b880d648416397210ec1c85
Fabricio Asat <fabricioasat00@gmail.com>
2023-05-02 18:42:33 -0300

    fix error - devolvía la contraseña

--- server/controllers/user/register.js
+++ server/controllers/user/register.js
@@ -9,6 +9,8 @@ export const register = async (req, res) => {
 	const emailCheck = await User.findOne({ email });
 	if (!!emailCheck) return res.status(400).send({ message: "El email ya está en uso" });
 	if (!emailValidator(email)) return res.status(400).send({ message: "Email inválido" });
+	if (password < 5) return res.status(400).send({ message: "Contraseña inválida" });
+	if (username < 3) return res.status(400).send({ message: "Nombre de usuario inválido" });
 
 	// Encriptado de contraseñas
 	const hashedPassword = await encrypt.hash(password, 10);
@@ -18,7 +20,8 @@ export const register = async (req, res) => {
 		password: hashedPassword,
 	});
 
-	delete newUser.password;
+	const sanitizedUser = newUser.toObject();
+	delete sanitizedUser.password;
 
-	return res.status(200).send({ message: "Usuario creado", response: newUser });
+	return res.status(200).send({ message: "Usuario creado", response: sanitizedUser });
 };
